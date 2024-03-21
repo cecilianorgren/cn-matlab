29955039f80a1b524b8cbcc7900596a52fd924cc
Cecilia Norgren <cecilia.norgren@uib.no>
2024-03-21 17:01:27 +0100

    tiny

--- mms_book/burch2016_half_dome.m
+++ mms_book/burch2016_half_dome.m
@@ -42,9 +42,11 @@ c_eval('ePDist? = mms.get_data(''PDe_fpi_brst_l2'',tint,?);',ic) % missing some 
 
 toc
 
-L = [0.3665, -0.1201, 0.9226];
-M = [0.5694, -0.7553, -0.3245];
-N = [0.7358, 0.6443, -0.2084];
+Lgse = [0.3665, -0.1201, 0.9226];
+Mgse = [0.5694, -0.7553, -0.3245];
+Ngse = [0.7358, 0.6443, -0.2084];
+
+
 %% Plot distributions
 ic = 2;
 times_utc = ['2015-10-16T13:07:02.160Z';...
@@ -206,7 +208,8 @@ c_eval('dmpaB = dmpaB?;',ic)
 c_eval('dslE = dslE?;',ic)
 c_eval('scPot = scPot?;',ic)
 
-delete(h)
+
+%if exist('h','var'); delete(h)
 
 nrows = 1;
 ncols = nt;
@@ -214,132 +217,151 @@ h = setup_subplots(nrows,ncols,'vertical');
 isub = 1;
 %nt = 1;
 
+set(gcf,'defaultLegendAutoUpdate','off');
+
 for it = 1:nt % 3D isosurface
     
   hca = h(isub); isub = isub + 1;
   time = times(it);
   elimit = 40;
   elimit = 35;
-  pdist = PDist.elim([elimit Inf]).tlim(time+0.499*0.03*[-1 1]);  
-  %scP = scPot.tlim(time+0.5*0.03*[-1 1]); scP = mean(scP.data,1);
+  %pdist = PDist.elim([elimit Inf]).tlim(time+0.499*0.03*[-1 1]);  
+  pdist = PDist.tlim(time+0.499*0.03*[-1 1]);  
+  pdist.data(:,1:5,:,:) = 3e-27;
+
+
+  scP = scPot.tlim(time+0.5*0.03*[-1 1]); scP = mean(scP.data,1);
   B = dmpaB.tlim(time+0.5*0.03*[-1 1]); B = mean(B.data,1); B = B/norm(B);
   E = dslE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); E = E/norm(E);
   Eperp = cross(B,cross(E,B));
   ExB = cross(E,B);
   
-  F = squeeze(pdist.data);
-  [VX,VY,VZ] = pdist.v;
-  VX = squeeze(VX);
-  VY = squeeze(VY);
-  VZ = squeeze(VZ);
-  if 1 % make circular in azimuth
-    %F = cat(2,[F(:,end,:),F,F(:,1,:)]);
-    %VX = cat(2,[VX(:,end,:),VX,VX(:,1,:)]);
-    %VY = cat(2,[VY(:,end,:),VY,VY(:,1,:)]);
-    %VZ = cat(2,[VZ(:,end,:),VZ,VZ(:,1,:)]);
-    F = cat(2,[F,F(:,1,:)]);
-    VX = cat(2,[VX,VX(:,1,:)]);
-    VY = cat(2,[VY,VY(:,1,:)]);
-    VZ = cat(2,[VZ,VZ(:,1,:)]);
-  end
-  if 0 % interpolate to close circle
-    
-  end
-  
-  % Rebin to square grid, for smoothing purposes
-  if 0
-    MP = pdist.macroparticles('skipzero',1,'ntot',1e6);
-    vmax = 100e3;
-    dv = 1000;
-    vx = -vmax:dv:vmax;
-    vy = -vmax:dv:vmax;
-    vz = -vmax:dv:vmax;
-    [VXg,VYg,VZg] = ndgrid(vx,vy,vz);
-    %[count edges mid loc] = histcn([VX(:),VY(:),VZ(:)],vx,vy,vz,'fun',@sum,'accumdata',MP.dn);  
-    [count edges mid loc] = histcn([MP.vx,MP.vy,MP.vz],vx,vy,vz,'fun',@sum,'accumdata',MP.dn);  
-  
-    F = count;
-    VX = mid{1};
-    VY = mid{2};
-    VZ = mid{3};
-  end
-%
-  nsmooth = 3;
-  F = smooth3(F,'box',nsmooth);
-  Flev = prctile(F(:),85);
-  Flev = 1e-26;
-  
-  Flev = 1e-27;
-  Flev = prctile(F(:),85);
-  Flev = 6e-27;
-  s = isosurface(VX,VY,VZ,F,Flev);
-  %s = isocaps(VX,VY,VZ,F,Flev);
-  p = patch(hca,Faces=s.faces,Vertices=s.vertices);
-  set(p,FaceColor=[0.5 1 0.5])
-  set(p,EdgeColor="none")
-  set(p,FaceAlpha=0.8)
-  camlight(hca,0,-45)
-  lighting(hca,'gouraud')
-
+  nSmooth = 3;
   if 1
-    hold(hca,'on')
-    Flev2 = 1e-27;
-    s2 = isosurface(VX,VY,VZ,F,Flev2);
-    p2 = patch(hca,Faces=s2.faces,Vertices=s2.vertices);
-    set(p2,FaceColor=[1 0.5 0.5])
-    set(p2,EdgeColor="none")
-    set(p2,FaceAlpha=0.3)  
-    hold(hca,'off')
-  end
-  set(hca,'colororder',[0.5 1 0.5; 1 0.5 0.5])
-  irf_legend(hca,{sprintf('f = %g',Flev),sprintf('f = %g',Flev2)}',[0.98 0.98],'backgroundcolor',[1 1 1],'fontsize',12)
-
-  if 0
-    pdist_low = PDist.elim([0 elimit]).tlim(time+0.499*0.03*[-1 1]);  
-    F = squeeze(pdist_low.data);
-    F = smooth3(F,'box',nsmooth);
-    [VX,VY,VZ] = pdist_low.v;
+    iso_values = [2e-27, 6e-27];
+    set(hca,'colororder',[1 0.5 0.5; 0.5 1 0.5]) 
+    T = [L;M;N];
+    hs = pdist.plot_isosurface(hca,'val',iso_values,'smooth',nSmooth,'fill','rotate',T);
+    
+    hs.Patch(1).FaceAlpha = 0.3;
+    hs.Patch(2).FaceAlpha = 0.9;
+    
+    legs = arrayfun(@(x)sprintf('%g',x),iso_values,'UniformOutput',false);
+    hleg = legend(hs.Patch,legs,'location','northeast');
+    hleg.Title.String = sprintf('f_%s (%s)',pdist.species(1),pdist.units);
+  else
+    F = squeeze(pdist.data);
+    [VX,VY,VZ] = pdist.v('scpot',scP*0);
     VX = squeeze(VX);
     VY = squeeze(VY);
     VZ = squeeze(VZ);
-    hold(hca,'on')
-    s2 = isosurface(VX,VY,VZ,F,1e-27);
-    p2 = patch(hca,Faces=s2.faces,Vertices=s2.vertices);
-    set(p2,FaceColor=[0.5 0.5 1])
-    set(p2,EdgeColor="none")
-    set(p2,FaceAlpha=1)  
-    hold(hca,'off')
+    if 1 % make circular in azimuth
+      %F = cat(2,[F(:,end,:),F,F(:,1,:)]);
+      %VX = cat(2,[VX(:,end,:),VX,VX(:,1,:)]);
+      %VY = cat(2,[VY(:,end,:),VY,VY(:,1,:)]);
+      %VZ = cat(2,[VZ(:,end,:),VZ,VZ(:,1,:)]);
+      F = cat(2,[F,F(:,1,:)]);
+      VX = cat(2,[VX,VX(:,1,:)]);
+      VY = cat(2,[VY,VY(:,1,:)]);
+      VZ = cat(2,[VZ,VZ(:,1,:)]);
+    end
+    if 0 % interpolate to close circle
+      
+    end    
+    if 0 % Rebin to square grid, for smoothing purposes
+      MP = pdist.macroparticles('skipzero',1,'ntot',1e6);
+      vmax = 100e3;
+      dv = 1000;
+      vx = -vmax:dv:vmax;
+      vy = -vmax:dv:vmax;
+      vz = -vmax:dv:vmax;
+      [VXg,VYg,VZg] = ndgrid(vx,vy,vz);
+      %[count edges mid loc] = histcn([VX(:),VY(:),VZ(:)],vx,vy,vz,'fun',@sum,'accumdata',MP.dn);  
+      [count edges mid loc] = histcn([MP.vx,MP.vy,MP.vz],vx,vy,vz,'fun',@sum,'accumdata',MP.dn);  
+    
+      F = count;
+      VX = mid{1};
+      VY = mid{2};
+      VZ = mid{3};
+    end
+  %    
+    F = smooth3(F,'box',nSmooth);
+    Flev = prctile(F(:),85);
+    Flev = 1e-26;
+    
+    Flev = 1e-27;
+    Flev = prctile(F(:),85);
+    Flev = 6e-27;
+    s = isosurface(VX,VY,VZ,F,Flev);
+    %s = isocaps(VX,VY,VZ,F,Flev);
+    p = patch(hca,Faces=s.faces,Vertices=s.vertices);
+    set(p,FaceColor=[0.5 1 0.5])
+    set(p,EdgeColor="none")
+    set(p,FaceAlpha=0.8)
+    camlight(hca,0,-45)
+    lighting(hca,'gouraud')
+
+    if 1
+      hold(hca,'on')
+      Flev2 = 1e-27;
+      s2 = isosurface(VX,VY,VZ,F,Flev2);
+      p2 = patch(hca,Faces=s2.faces,Vertices=s2.vertices);
+      set(p2,FaceColor=[1 0.5 0.5])
+      set(p2,EdgeColor="none")
+      set(p2,FaceAlpha=0.3)  
+      hold(hca,'off')
+    end
+    set(hca,'colororder',[0.5 1 0.5; 1 0.5 0.5])
+    irf_legend(hca,{sprintf('f = %g',Flev),sprintf('f = %g',Flev2)}',[0.98 0.98],'backgroundcolor',[1 1 1],'fontsize',12)
+
+    if 0
+      pdist_low = PDist.elim([0 elimit]).tlim(time+0.499*0.03*[-1 1]);  
+      F = squeeze(pdist_low.data);
+      F = smooth3(F,'box',nSmooth);
+      [VX,VY,VZ] = pdist_low.v;
+      VX = squeeze(VX);
+      VY = squeeze(VY);
+      VZ = squeeze(VZ);
+      hold(hca,'on')
+      s2 = isosurface(VX,VY,VZ,F,1e-27);
+      p2 = patch(hca,Faces=s2.faces,Vertices=s2.vertices);
+      set(p2,FaceColor=[0.5 0.5 1])
+      set(p2,EdgeColor="none")
+      set(p2,FaceAlpha=1)  
+      hold(hca,'off')
+    end
+  
+    %[faces,verts] = extractIsosurface(F,Flev);
+  
+
+    view(hca,[1 1 1])
+    %%
+    % figure
+    % p = patch(Faces=faces,Vertices=verts);
+    % isonormals(V,p)
+    % view(3)
+    % set(p,FaceColor=[0.5 1 0.5])
+    % set(p,EdgeColor="none")
+    % camlight
+    % lighting gouraud
+  
+    %colormap(hca,irf_colormap('waterfall'))
+    %hca.XLim = vlim_red;
+    %hca.YLim = vlim_red;
+    hca.XLabel.String = 'v_{x} (km/s)';
+    hca.YLabel.String = 'v_{y} (km/s)';
+    hca.ZLabel.String = 'v_{z} (km/s)';  
+  
+    axis(hca,'square')
+    axis(hca,'equal')
+    grid(hca,'on')
+    hca.Title.String = {'Box averaged over',sprintf('%g (spherical) datapoints',nSmooth),pdist.time.utc};
   end
   
-  %[faces,verts] = extractIsosurface(F,Flev);
-  
-
-  view(hca,[1 1 1])
-  %%
-  % figure
-  % p = patch(Faces=faces,Vertices=verts);
-  % isonormals(V,p)
-  % view(3)
-  % set(p,FaceColor=[0.5 1 0.5])
-  % set(p,EdgeColor="none")
-  % camlight
-  % lighting gouraud
-
-  %colormap(hca,irf_colormap('waterfall'))
-  %hca.XLim = vlim_red;
-  %hca.YLim = vlim_red;
-  hca.XLabel.String = 'v_{x} (km/s)';
-  hca.YLabel.String = 'v_{y} (km/s)';
-  hca.ZLabel.String = 'v_{z} (km/s)';  
-
-  axis(hca,'square')
-  axis(hca,'equal')
-  grid(hca,'on')
-  hca.Title.String = {'Box averaged over',sprintf('%g (spherical) datapoints',nsmooth),pdist.time.utc};
   if 1 % plot E, ExB, B
     %%
     hold(hca,'on')
-    qmax = 9000; 
+    qmax = 10000; 
     nE = E/norm(E);
     nEperp = Eperp/norm(Eperp);
     nB = B/norm(B);
@@ -347,7 +369,14 @@ for it = 1:nt % 3D isosurface
     colors_quiv_B = [0 0 0];
     colors_quiv_E = [1 0 0];
     colors_quiv_Eperp = [1 0.5 0.5];
-    colors_quiv_ExB = [0 0.5 1];    
+    colors_quiv_ExB = [0 0.5 1]; 
+
+    if 1
+      nE = nE*hs.Rotation';
+      nExB = nExB*hs.Rotation';
+      nB = nB*hs.Rotation';
+      nEperp = nEperp*hs.Rotation';
+    end
     
     fontsize = 13;
     fontweight = 'bold';
@@ -369,16 +398,23 @@ for it = 1:nt % 3D isosurface
   end
   if 1 % plot LMN (obs in GSE!!)
     %%
+     if 1
+      L_ = L*hs.Rotation';
+      M_ = M*hs.Rotation';
+      N_ = N*hs.Rotation';
+      
+     end
+
     hold(hca,'on')
     colors_quiv_LMN = [0.5 0.5 0.5];
-    quiver3(hca,qmax*L(1)*-1,qmax*L(2)*-1,qmax*L(3)*-1,qmax*L(1)*2,qmax*L(2)*2,qmax*L(3)*2,'color',colors_quiv_LMN,'linewidth',2)
-    text(hca,qmax*L(1)*1,qmax*L(2)*1,qmax*L(3)*1,'L','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+    quiver3(hca,qmax*L_(1)*-1,qmax*L_(2)*-1,qmax*L_(3)*-1,qmax*L_(1)*2,qmax*L_(2)*2,qmax*L_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*L_(1)*1,qmax*L_(2)*1,qmax*L_(3)*1,'L','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
 
-    quiver3(hca,qmax*M(1)*-1,qmax*M(2)*-1,qmax*M(3)*-1,qmax*M(1)*2,qmax*M(2)*2,qmax*M(3)*2,'color',colors_quiv_LMN,'linewidth',2)
-    text(hca,qmax*M(1)*1,qmax*M(2)*1,qmax*M(3)*1,'M','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+    quiver3(hca,qmax*M_(1)*-1,qmax*M_(2)*-1,qmax*M_(3)*-1,qmax*M_(1)*2,qmax*M_(2)*2,qmax*M_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*M_(1)*1,qmax*M_(2)*1,qmax*M_(3)*1,'M','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
 
-    quiver3(hca,qmax*N(1)*-1,qmax*N(2)*-1,qmax*N(3)*-1,qmax*N(1)*2,qmax*N(2)*2,qmax*N(3)*2,'color',colors_quiv_LMN,'linewidth',2)
-    text(hca,qmax*N(1)*1,qmax*N(2)*1,qmax*N(3)*1,'N','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+    quiver3(hca,qmax*N_(1)*-1,qmax*N_(2)*-1,qmax*N_(3)*-1,qmax*N_(1)*2,qmax*N_(2)*2,qmax*N_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*N_(1)*1,qmax*N_(2)*1,qmax*N_(3)*1,'N','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
 
     hold(hca,'off')
 
@@ -396,7 +432,9 @@ if 1
   hlight = findobj(h,'type','light');
   delete(hlight)
   for ip = 1:nt
-    camlight(h(ip),45,45);
+    %camlight(h(ip),45,45);
+    camlight(h(ip),45,10);
+    camlight(h(ip),45,190);
   end
 end
 
@@ -441,5 +479,229 @@ if 0
   text(X,zeros(size(X)),zeros(size(X))-3.*Zoff,XL);
   text(zeros(size(Y))-3.*Xoff,Y,zeros(size(Y)),YL);
   text(zeros(size(Z))-3.*Xoff,zeros(size(Z)),Z,ZL);
+  hold(gca,'off')
+end
+%% Plot isosurfaces of distribution, PDist, cleaned
+units = irf_units;
+ic = 2;
+times_utc = ['2015-10-16T13:07:02.160Z';...
+             '2015-10-16T13:07:02.190Z';...
+             '2015-10-16T13:07:02.220Z';...
+             '2015-10-16T13:07:02.250Z';...
+             '2015-10-16T13:07:02.280Z'];
+times = irf_time(times_utc,'utc>EpochTT');
+%times = times + 0.090;
+times = times(1:3);
+nt = times.length;
+
+vint_red = [-1000 1000];
+vint = [3500 5000];
+vint = [3000 5000] + 500;
+eint = [50 90];
+vlim_red = [-10 10];
+eint = units.me*(vint*1e3).^2/2/units.eV;
+c_eval('PDist = ePDist?;',ic)
+c_eval('dmpaB = dmpaB?;',ic)
+c_eval('dslE = dslE?;',ic)
+c_eval('gseE = gseE?;',ic)
+c_eval('scPot = scPot?;',ic)
+
+
+%if exist('h','var'); delete(h)
+
+nrows = 1;
+ncols = nt;
+h = setup_subplots(nrows,ncols,'vertical');
+isub = 1;
+%nt = 1;
+
+set(gcf,'defaultLegendAutoUpdate','off');
+
+for it = 1:nt % 3D isosurface
+    
+  hca = h(isub); isub = isub + 1;
+  time = times(it);
+  elimit = 40;
+  elimit = 35;
+  %pdist = PDist.elim([elimit Inf]).tlim(time+0.499*0.03*[-1 1]);  
+  pdist = PDist.tlim(time+0.499*0.03*[-1 1]);  
+  pdist.data(:,1:5,:,:) = 3e-27;
+
+
+  scP = scPot.tlim(time+0.5*0.03*[-1 1]); scP = mean(scP.data,1);
+  B = dmpaB.tlim(time+0.5*0.03*[-1 1]); B = mean(B.data,1); Bn = B/norm(B);
+  E = dslE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
+  E = gseE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
+  Eperp = cross(Bn,cross(E,Bn)); Eperpn = Eperp\norm(Eperp);
+  ExB = cross(E,B); ExBn = Eperp\norm(Eperp);
+  
+  nSmooth = 3;
+  
+  iso_values = [2e-27, 6e-27];
+  iso_values = iso_values(2); 
+  set(hca,'colororder',[1 0.5 0.5; 0.5 1 0.5]) 
+  set(hca,'colororder',[0.2 0.5 1; 1 0.5 0.5; 0.5 1 0.5]) 
+  T = [L;M;N];
+  hs = pdist.plot_isosurface(hca,'val',iso_values,'smooth',nSmooth,'fill','rotate',T);
+  
+  hs.Patch(1).FaceAlpha = 0.9;
+%  hs.Patch(1).FaceAlpha = 0.3;
+%  hs.Patch(2).FaceAlpha = 0.9;
+  
+  legs = arrayfun(@(x)sprintf('%g',x),iso_values,'UniformOutput',false);
+  hleg = legend(hs.Patch,legs,'location','northeast');
+  hleg.Title.String = sprintf('f_%s (%s)',pdist.species(1),pdist.units);
+  
+  
+  if 1 % plot E, ExB, B
+    %%
+    hold(hca,'on')
+    qmax = 10000; 
+    nE = E/norm(E);
+    nEperp = Eperp/norm(Eperp);
+    nB = B/norm(B);
+    nExB = ExB/norm(ExB);
+    colors_quiv_B = [0 0 0];
+    colors_quiv_E = [1 0 0];
+    colors_quiv_Eperp = [1 0.5 0.5];
+    colors_quiv_ExB = [0 0.5 1]; 
+
+    if 1
+      nE = nE*hs.Rotation';
+      nExB = nExB*hs.Rotation';
+      nB = nB*hs.Rotation';
+      nEperp = nEperp*hs.Rotation';
+    end
+    
+    fontsize = 13;
+    fontweight = 'bold';
+    quiver3(hca,qmax*nB(1)*-1,qmax*nB(2)*-1,qmax*nB(3)*-1,qmax*nB(1)*2,qmax*nB(2)*2,qmax*nB(3)*2,'color',colors_quiv_B,'linewidth',2)
+    %text(hca,qmax*nB(1)*1,qmax*nB(2)*1,qmax*nB(3)*1,'B','color',colors_quiv_B,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nB(1)*1,qmax*nB(2)*1,qmax*nB(3)*1,sprintf('B=%.0fnT',norm(B)),'color',colors_quiv_B,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*nE(1)*-1,qmax*nE(2)*-1,qmax*nE(3)*-1,qmax*nE(1)*2,qmax*nE(2)*2,qmax*nE(3)*2,'color',colors_quiv_E,'linewidth',2)
+    %text(hca,qmax*nE(1)*1,qmax*nE(2)*1,qmax*nE(3)*1,'E','color',colors_quiv_E,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nE(1)*1,qmax*nE(2)*1,qmax*nE(3)*1,sprintf('E=%.0fmV/m',norm(E)),'color',colors_quiv_E,'fontweight',fontweight,'fontsize',fontsize)
+    
+    quiver3(hca,qmax*nEperp(1)*-1,qmax*nEperp(2)*-1,qmax*nEperp(3)*-1,qmax*nEperp(1)*2,qmax*nEperp(2)*2,qmax*nEperp(3)*2,'color',colors_quiv_Eperp,'linewidth',2)
+    %text(hca,qmax*nEperp(1)*1,qmax*nEperp(2)*1,qmax*nEperp(3)*1,'E_\perp','color',colors_quiv_Eperp,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nEperp(1)*1,qmax*nEperp(2)*1,qmax*nEperp(3)*1,['E_\perp' sprintf('=%.0fmV/m',norm(Eperp))],'color',colors_quiv_Eperp,'fontweight',fontweight,'fontsize',fontsize)
+    
+    quiver3(hca,qmax*nExB(1)*-1,qmax*nExB(2)*-1,qmax*nExB(3)*-1,qmax*nExB(1)*2,qmax*nExB(2)*2,qmax*nExB(3)*2,'color',colors_quiv_ExB,'linewidth',2)    
+    text(hca,qmax*nExB(1)*1,qmax*nExB(2)*1,qmax*nExB(3)*1,'E\times{B}','color',colors_quiv_ExB,'fontweight',fontweight,'fontsize',fontsize)
+    %text(hca,qmax*nExB(1)*1,qmax*nExB(2)*1,qmax*nExB(3)*1,sprintf('E\times{B}=%.0fmV/m',norm(Eperp)),'color',colors_quiv_ExB,'fontweight',fontweight,'fontsize',fontsize)
+
+    hold(hca,'off')
+    %set(hca,'colororder',colors_quiv)
+    %irf_legend(hca,{'B','E','E_\perp','E\times{B}'},[0.98 0.98])
+  end
+  if 0 % plot LMN (obs in GSE!!)
+    %%
+     if 1
+      L_ = L*hs.Rotation';
+      M_ = M*hs.Rotation';
+      N_ = N*hs.Rotation';
+      
+     end
+
+    hold(hca,'on')
+    colors_quiv_LMN = [0.5 0.5 0.5];
+    quiver3(hca,qmax*L_(1)*-1,qmax*L_(2)*-1,qmax*L_(3)*-1,qmax*L_(1)*2,qmax*L_(2)*2,qmax*L_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*L_(1)*1,qmax*L_(2)*1,qmax*L_(3)*1,'L','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*M_(1)*-1,qmax*M_(2)*-1,qmax*M_(3)*-1,qmax*M_(1)*2,qmax*M_(2)*2,qmax*M_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*M_(1)*1,qmax*M_(2)*1,qmax*M_(3)*1,'M','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*N_(1)*-1,qmax*N_(2)*-1,qmax*N_(3)*-1,qmax*N_(1)*2,qmax*N_(2)*2,qmax*N_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*N_(1)*1,qmax*N_(2)*1,qmax*N_(3)*1,'N','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    hold(hca,'off')
+
+  end
+end
+
+for ip = 1:numel(h)
+  h(ip).XLabel.String = 'v_L (km/s)';
+  h(ip).YLabel.String = 'v_M (km/s)';
+  h(ip).ZLabel.String = 'v_N (km/s)';
+  vlim = [-9 9]*1e3;
+  h(ip).XLim = vlim;
+  h(ip).YLim = vlim;
+  h(ip).ZLim = vlim;
+  h(ip).XTick = [-10:5:10]*1e3;
+  h(ip).YTick = [-10:5:10]*1e3;
+  h(ip).ZTick = [-10:5:10]*1e3;
+  h(ip).XGrid = 'on';
+  h(ip).YGrid = 'on';
+  h(ip).ZGrid = 'on';
+  h(ip).Box = 'on';
+end
+
+hlinks = linkprop(h,{'view'});
+c_eval('h(?).FontSize = 12;',1:numel(h))
+
+%view([0 0 1])
+
+if 1 % Reset camlight
+  %%
+  hlight = findobj(h,'type','light');
+  delete(hlight)
+  for ip = 1:nt
+    %camlight(h(ip),45,45);
+    camlight(h(ip),45,10);
+    camlight(h(ip),45,190);
+  end
+end
+
+if 0 % Put axis at origin, missing labels, i.e. v_L (km/s)
+  %%
+  set(gca,'Visible','off')
+  %c_eval('h(?).XTick = [-10:2:10]*1e3;',1:numel(h))
+  %c_eval('h(?).YTick = [-10:2:10]*1e3;',1:numel(h))
+  %c_eval('h(?).ZTick = [-10:2:10]*1e3;',1:numel(h))
+  hold(gca,'on')
+  % DRAW AXIS LINEs
+  plot3(get(gca,'XLim'),[0 0],[0 0],'k');
+  plot3([0 0],[0 0],get(gca,'ZLim'),'k');
+  plot3([0 0],get(gca,'YLim'),[0 0],'k');
+  % GET TICKS
+  X=get(gca,'Xtick');
+  Y=get(gca,'Ytick');
+  Z=get(gca,'Ztick');
+  % GET LABELS
+  XL=get(gca,'XtickLabel');
+  YL=get(gca,'YtickLabel');
+  ZL=get(gca,'ZtickLabel');
+  % REMOVE TICKS
+  set(gca,'Xtick',[]);
+  set(gca,'Ytick',[]);
+  set(gca,'Ztick',[]);
+  % GET OFFSETS
+  Xoff=diff(get(gca,'XLim'))./30;
+  Yoff=diff(get(gca,'YLim'))./30;
+  Zoff=diff(get(gca,'ZLim'))./30;
+  % DRAW TICKS
+  %%%%%%% THIS COULD BE VECTORiZeD %%%%%%%
+  for i=1:length(X)
+     plot3([X(i) X(i)],[0 0],[-Zoff Zoff],'k');
+  end;
+  for i=1:length(Y)
+     plot3([-Xoff Xoff],[Y(i) Y(i)],[0 0],'k');
+  end;
+  for i=1:length(Z)
+     plot3([-Xoff Xoff],[0 0],[Z(i) Z(i)],'k');
+  end;
+  % DRAW LABELS
+  text(X,zeros(size(X)),zeros(size(X))-3.*Zoff,XL);
+  text(zeros(size(Y))-3.*Xoff,Y,zeros(size(Y)),YL);
+  text(zeros(size(Z))-3.*Xoff,zeros(size(Z)),Z,ZL);
+
+  xlim_ = get(gca,'XLim');
+  ylim_ = get(gca,'YLim');
+  zlim_ = get(gca,'ZLim');
+  text(gca,xlim_(2),0,0,'L (km/s)')
+  text(gca,0,ylim_(2),0,'M (km/s)')
+  text(gca,0,0,zlim_(2),'N (km/s)')
+  
   hold(gca,'off')
 end
