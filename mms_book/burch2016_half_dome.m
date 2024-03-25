3e5ff8f897bb970551bf3f804b37ccb75b29141f
Cecilia Norgren <cecilia.norgren@uib.no>
2024-03-25 17:52:42 +0100

    tiny

--- mms_book/burch2016_half_dome.m
+++ mms_book/burch2016_half_dome.m
@@ -1,15 +1,15 @@
 %% Load data
 % units = irf_units;
 irf.log('critical')
-ic = 2;
+ic = 3;
 
 localuser = 'cno062';
-%localuser = 'cecilia';
+localuser = 'cecilia';
 %mms.db_init('local_file_db','/Volumes/Fountain/Data/MMS');
 %mms.db_init('local_file_db','/Users/cecilia/Data/MMS');
 mms.db_init('local_file_db',['/Users/' localuser '/Data/MMS']);
 %mms.db_init('local_file_db',['/Volumes/DataRaid/MMS']);
-%mms.db_init('local_file_db',['/Volumes/mms']);
+mms.db_init('local_file_db',['/Volumes/mms']);
 db_info = datastore('mms_db');   
 
 tint_all = irf.tint('2015-10-16T13:05:24.00Z/2015-10-16T13:07:30.00Z');
@@ -491,9 +491,9 @@ if 0
   text(zeros(size(Z))-3.*Xoff,zeros(size(Z)),Z,ZL);
   hold(gca,'off')
 end
-%% Plot isosurfaces of distribution, PDist, cleaned
+%% Plot isosurfaces of distribution, PDist, cleaned, also with TSeries of locations
 units = irf_units;
-ic = 3;
+ic = 2;
 times_utc = ['2015-10-16T13:07:02.160Z';...
              '2015-10-16T13:07:02.190Z';...
              '2015-10-16T13:07:02.220Z';...
@@ -501,13 +501,15 @@ times_utc = ['2015-10-16T13:07:02.160Z';...
              '2015-10-16T13:07:02.280Z'];
 times = irf_time(times_utc,'utc>EpochTT');
 
-times = EpochTT(['2017-07-11T22:34:01.300000000Z';
-  '2017-07-11T22:34:01.800000000Z';...
-  '2017-07-11T22:34:02.300000000Z';...
-  '2017-07-11T22:34:02.800000000Z';...
-  '2017-07-11T22:34:03.300000000Z']);
+%times = EpochTT(['2017-07-11T22:34:01.300000000Z';
+%  '2017-07-11T22:34:01.800000000Z';...
+%  '2017-07-11T22:34:02.300000000Z';...
+%  '2017-07-11T22:34:02.800000000Z';...
+%  '2017-07-11T22:34:03.300000000Z']);
 %times = times + 0.090;
-times = times(3:5);
+times = times(1:3);
+
+tint_plot = [times.start times.stop] + [-0.5 0.5];
 nt = times.length;
 
 vint_red = [-1000 1000];
@@ -528,7 +530,62 @@ c_eval('scPot = scPot?;',ic)
 
 nrows = 1;
 ncols = nt;
-h = setup_subplots(nrows,ncols,'vertical');
+%h = setup_subplots(nrows,ncols,'vertical');
+[h1,h] = initialize_combined_plot('topbottom',3,nrows,ncols,0.5,'horizontal');
+
+isub = 1;
+
+hca = h1(isub); isub = isub + 1;
+set(hca,'ColorOrder',mms_colors('xyza'))
+c_eval('plB = gseB?*Tgse'';',ic)
+irf_plot(hca,plB)
+hca.YLabel.String = 'B_{LMN} (nT)';  hca.YLabel.Interpreter = 'tex';
+set(hca,'ColorOrder',mms_colors('xyza'))
+irf_legend(hca,{'L','M','N'},[0.98 0.98])
+
+hca = h1(isub); isub = isub + 1;
+set(hca,'ColorOrder',mms_colors('xyz'))
+c_eval('plE = gseE?*Tgse'';',ic)
+irf_plot(hca,plE)
+set(hca,'ColorOrder',mms_colors('xyz'))
+hold(hca,'on')
+irf_plot(hca,plE.resample(PDist,'mean'))
+hold(hca,'off')
+hca.YLabel.String = 'E_{LMN} (mV/m)';  hca.YLabel.Interpreter = 'tex';
+set(hca,'ColorOrder',mms_colors('xyz'))
+irf_legend(hca,{'L','M','N'},[0.98 0.98])
+
+hca = h1(isub); isub = isub + 1;
+set(hca,'ColorOrder',mms_colors('xyz'))
+c_eval('plE = gseE?*Tgse'';',ic)
+irf_plot(hca,plE.resample(PDist.time+0.015))
+hca.YLabel.String = 'E_{LMN} (mV/m)';  hca.YLabel.Interpreter = 'tex';
+set(hca,'ColorOrder',mms_colors('xyz'))
+irf_legend(hca,{'L','M','N'},[0.98 0.98])
+
+if 0
+  hca = h1(isub); isub = isub + 1;
+  set(hca,'ColorOrder',mms_colors('xyza'))
+  c_eval('plVe = gseVe?*Tgse'';',ic)
+  irf_plot(hca,plVe)
+  hca.YLabel.String = 'V_{e,LMN} (mV/m)';  hca.YLabel.Interpreter = 'tex';
+  set(hca,'ColorOrder',mms_colors('xyza'))
+  irf_legend(hca,{'L','M','N'},[0.98 0.98])
+end
+
+irf_zoom(h1,'x',tint_plot)
+irf_zoom(h1,'y')
+
+colors = get(gca,'colororder');
+for it = 1:nt % mark locations of distributions
+  for ip = 1:numel(h1)
+    c_eval('tcenter = ePDist?.tlim(times(it)+0.03*0.49*[-1 1]).time;',ic)
+    time_int = tcenter+0.03*0.5*[-1 1];
+    irf_pl_mark(h1(ip),time_int',colors(it,:))
+  end
+end
+
+
 isub = 1;
 %nt = 1;
 
@@ -542,30 +599,31 @@ for it = 1:nt % 3D isosurface
   elimit = 35;
   elimit = 30;
   pdist = PDist.elim([elimit Inf]).tlim(time+0.499*0.03*[-1 1]);  
-  %pdist = PDist.tlim(time+0.499*0.03*[-1 1]);  
-  %pdist.data(:,1:5,:,:) = 3e-27;
+  pdist = PDist.tlim(time+0.499*0.03*[-1 1]);  
+  pdist.data(:,1:5,:,:) = 3e-27;
 
-
-  scP = scPot.tlim(time+0.5*0.03*[-1 1]); scP = mean(scP.data,1);
-  B = dmpaB.tlim(time+0.5*0.03*[-1 1]); B = mean(B.data,1); Bn = B/norm(B);
-  E = dslE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
-  %E = gseE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
+  tint_tmp = time+0.5*0.03*[-1 1];
+  scP = scPot.tlim(tint_tmp); scP = mean(scP.data,1);
+  B = dmpaB.tlim(tint_tmp); B = mean(B.data,1); Bn = B/norm(B);
+  E = dslE.tlim(tint_tmp); E = mean(E.data,1); En = E/norm(E);
+  %E = gseE.tlim(tint_tmp); E = mean(E.data,1); En = E/norm(E);
   Eperp = cross(Bn,cross(E,Bn)); Eperpn = Eperp\norm(Eperp);
   ExB = cross(E,B); ExBn = Eperp\norm(Eperp);
   
   nSmooth = 3;
   
   iso_values = [0.5e-27, 6e-27];
-  iso_values = [1.5e-30 22e-30];
-  %iso_values = iso_values(2); 
+  %iso_values = [1.5e-30 22e-30];
+  iso_values = iso_values(2); 
   set(hca,'colororder',[1 0.5 0.5; 0.5 1 0.5]) 
   set(hca,'colororder',[0.2 0.5 1; 1 0.5 0.5; 0.5 1 0.5]) 
+  %set(hca,'colororder',colors(it,:)) 
   %Tgse = [Lgse;Mgse;Ngse];
   c_eval('Tdsl = [tsLdsl?.resample(pdist).data;tsMdsl?.resample(pdist).data;tsNdsl?.resample(pdist).data];',ic)
   hs = pdist.plot_isosurface(hca,'val',iso_values,'smooth',nSmooth,'fill','rotate',Tdsl);
   %hs = pdist.plot_isosurface(hca,'smooth',nSmooth,'fill','rotate',Tdsl);
   
-  %hs.Patch(1).FaceAlpha = 0.9;
+  hs.Patch(1).FaceAlpha = 0.9;  
   %hs.Patch(1).FaceAlpha = 0.3;
   %hs.Patch(2).FaceAlpha = 0.9;
   
@@ -645,7 +703,243 @@ for ip = 1:numel(h)
   h(ip).YLabel.String = 'v_M (km/s)';
   h(ip).ZLabel.String = 'v_N (km/s)';
   vlim = [-9 9]*1e3;
-  vlim = 31*[-1 1]*1e3;
+  vlim = 10*[-1 1]*1e3;
+  h(ip).XLim = vlim;
+  h(ip).YLim = vlim;
+  h(ip).ZLim = vlim;
+  h(ip).XTick = [-10:5:10]*1e3;
+  h(ip).YTick = [-10:5:10]*1e3;
+  h(ip).ZTick = [-10:5:10]*1e3;
+  h(ip).XGrid = 'on';
+  h(ip).YGrid = 'on';
+  h(ip).ZGrid = 'on';
+  h(ip).Box = 'on';
+end
+
+hlinks = linkprop(h,{'view'});
+c_eval('h(?).FontSize = 12;',1:numel(h))
+
+%view([0 0 1])
+
+if 1 % Reset camlight
+  %%
+  hlight = findobj(h,'type','light');
+  delete(hlight)
+  for ip = 1:nt
+    %camlight(h(ip),45,45);
+    camlight(h(ip),45,10);
+    camlight(h(ip),45,190);
+  end
+end
+
+if 0 % Put axis at origin, missing labels, i.e. v_L (km/s)
+  %%
+  set(gca,'Visible','off')
+  %c_eval('h(?).XTick = [-10:2:10]*1e3;',1:numel(h))
+  %c_eval('h(?).YTick = [-10:2:10]*1e3;',1:numel(h))
+  %c_eval('h(?).ZTick = [-10:2:10]*1e3;',1:numel(h))
+  hold(gca,'on')
+  % DRAW AXIS LINEs
+  plot3(get(gca,'XLim'),[0 0],[0 0],'k');
+  plot3([0 0],[0 0],get(gca,'ZLim'),'k');
+  plot3([0 0],get(gca,'YLim'),[0 0],'k');
+  % GET TICKS
+  X=get(gca,'Xtick');
+  Y=get(gca,'Ytick');
+  Z=get(gca,'Ztick');
+  % GET LABELS
+  XL=get(gca,'XtickLabel');
+  YL=get(gca,'YtickLabel');
+  ZL=get(gca,'ZtickLabel');
+  % REMOVE TICKS
+  set(gca,'Xtick',[]);
+  set(gca,'Ytick',[]);
+  set(gca,'Ztick',[]);
+  % GET OFFSETS
+  Xoff=diff(get(gca,'XLim'))./30;
+  Yoff=diff(get(gca,'YLim'))./30;
+  Zoff=diff(get(gca,'ZLim'))./30;
+  % DRAW TICKS
+  %%%%%%% THIS COULD BE VECTORiZeD %%%%%%%
+  for i=1:length(X)
+     plot3([X(i) X(i)],[0 0],[-Zoff Zoff],'k');
+  end;
+  for i=1:length(Y)
+     plot3([-Xoff Xoff],[Y(i) Y(i)],[0 0],'k');
+  end;
+  for i=1:length(Z)
+     plot3([-Xoff Xoff],[0 0],[Z(i) Z(i)],'k');
+  end;
+  % DRAW LABELS
+  text(X,zeros(size(X)),zeros(size(X))-3.*Zoff,XL);
+  text(zeros(size(Y))-3.*Xoff,Y,zeros(size(Y)),YL);
+  text(zeros(size(Z))-3.*Xoff,zeros(size(Z)),Z,ZL);
+
+  xlim_ = get(gca,'XLim');
+  ylim_ = get(gca,'YLim');
+  zlim_ = get(gca,'ZLim');
+  text(gca,xlim_(2),0,0,'L (km/s)')
+  text(gca,0,ylim_(2),0,'M (km/s)')
+  text(gca,0,0,zlim_(2),'N (km/s)')
+  
+  hold(gca,'off')
+end
+
+%% Plot isosurfaces of distribution, PDist, cleaned
+units = irf_units;
+ic = 2;
+times_utc = ['2015-10-16T13:07:02.160Z';...
+             '2015-10-16T13:07:02.190Z';...
+             '2015-10-16T13:07:02.220Z';...
+             '2015-10-16T13:07:02.250Z';...
+             '2015-10-16T13:07:02.280Z'];
+times = irf_time(times_utc,'utc>EpochTT');
+
+%times = EpochTT(['2017-07-11T22:34:01.300000000Z';
+%  '2017-07-11T22:34:01.800000000Z';...
+%  '2017-07-11T22:34:02.300000000Z';...
+%  '2017-07-11T22:34:02.800000000Z';...
+%  '2017-07-11T22:34:03.300000000Z']);
+%times = times + 0.090;
+times = times(1:3);
+nt = times.length;
+
+vint_red = [-1000 1000];
+vint = [3500 5000];
+vint = [3000 5000] + 500;
+eint = [50 90];
+eint = [50 90];
+vlim_red = [-10 10];
+eint = units.me*(vint*1e3).^2/2/units.eV;
+c_eval('PDist = ePDist?;',ic)
+c_eval('dmpaB = dmpaB?;',ic)
+c_eval('dslE = dslE?;',ic)
+c_eval('gseE = gseE?;',ic)
+c_eval('scPot = scPot?;',ic)
+
+
+%if exist('h','var'); delete(h)
+
+nrows = 1;
+ncols = nt;
+h = setup_subplots(nrows,ncols,'vertical');
+isub = 1;
+%nt = 1;
+
+set(gcf,'defaultLegendAutoUpdate','off');
+
+for it = 1:nt % 3D isosurface
+    
+  hca = h(isub); isub = isub + 1;
+  time = times(it);
+  elimit = 40;
+  elimit = 35;
+  elimit = 30;
+  pdist = PDist.elim([elimit Inf]).tlim(time+0.499*0.03*[-1 1]);  
+  pdist = PDist.tlim(time+0.499*0.03*[-1 1]);  
+  pdist.data(:,1:5,:,:) = 3e-27;
+
+
+  scP = scPot.tlim(time+0.5*0.03*[-1 1]); scP = mean(scP.data,1);
+  B = dmpaB.tlim(time+0.5*0.03*[-1 1]); B = mean(B.data,1); Bn = B/norm(B);
+  E = dslE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
+  %E = gseE.tlim(time+0.5*0.03*[-1 1]); E = mean(E.data,1); En = E/norm(E);
+  Eperp = cross(Bn,cross(E,Bn)); Eperpn = Eperp\norm(Eperp);
+  ExB = cross(E,B); ExBn = Eperp\norm(Eperp);
+  
+  nSmooth = 3;
+  
+  iso_values = [0.5e-27, 6e-27];
+  %iso_values = [1.5e-30 22e-30];
+  iso_values = iso_values(2); 
+  set(hca,'colororder',[1 0.5 0.5; 0.5 1 0.5]) 
+  set(hca,'colororder',[0.2 0.5 1; 1 0.5 0.5; 0.5 1 0.5]) 
+  %Tgse = [Lgse;Mgse;Ngse];
+  c_eval('Tdsl = [tsLdsl?.resample(pdist).data;tsMdsl?.resample(pdist).data;tsNdsl?.resample(pdist).data];',ic)
+  hs = pdist.plot_isosurface(hca,'val',iso_values,'smooth',nSmooth,'fill','rotate',Tdsl);
+  %hs = pdist.plot_isosurface(hca,'smooth',nSmooth,'fill','rotate',Tdsl);
+  
+  hs.Patch(1).FaceAlpha = 0.9;
+  %hs.Patch(1).FaceAlpha = 0.3;
+  %hs.Patch(2).FaceAlpha = 0.9;
+  
+  legs = arrayfun(@(x)sprintf('%g',x),hs.Values,'UniformOutput',false);
+  hleg = legend(hs.Patch,legs,'location','northeast');
+  hleg.Title.String = sprintf('f_%s (%s)',pdist.species(1),pdist.units);
+  
+  
+  if 1 % plot E, ExB, B    
+    hold(hca,'on')
+    qmax = 10000; 
+    nE = E/norm(E);
+    nEperp = Eperp/norm(Eperp);
+    nB = B/norm(B);
+    nExB = ExB/norm(ExB);
+    colors_quiv_B = [0 0 0];
+    colors_quiv_E = [1 0 0];
+    colors_quiv_Eperp = [1 0.5 0.5];
+    colors_quiv_ExB = [0 0.5 1]; 
+
+    if 1
+      nE = nE*hs.Rotation';
+      nExB = nExB*hs.Rotation';
+      nB = nB*hs.Rotation';
+      nEperp = nEperp*hs.Rotation';
+    end
+    
+    fontsize = 13;
+    fontweight = 'bold';
+    quiver3(hca,qmax*nB(1)*-1,qmax*nB(2)*-1,qmax*nB(3)*-1,qmax*nB(1)*2,qmax*nB(2)*2,qmax*nB(3)*2,'color',colors_quiv_B,'linewidth',2)
+    %text(hca,qmax*nB(1)*1,qmax*nB(2)*1,qmax*nB(3)*1,'B','color',colors_quiv_B,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nB(1)*1,qmax*nB(2)*1,qmax*nB(3)*1,sprintf('B=%.0fnT',norm(B)),'color',colors_quiv_B,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*nE(1)*-1,qmax*nE(2)*-1,qmax*nE(3)*-1,qmax*nE(1)*2,qmax*nE(2)*2,qmax*nE(3)*2,'color',colors_quiv_E,'linewidth',2)
+    %text(hca,qmax*nE(1)*1,qmax*nE(2)*1,qmax*nE(3)*1,'E','color',colors_quiv_E,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nE(1)*1,qmax*nE(2)*1,qmax*nE(3)*1,sprintf('E=%.0fmV/m',norm(E)),'color',colors_quiv_E,'fontweight',fontweight,'fontsize',fontsize)
+    
+    quiver3(hca,qmax*nEperp(1)*-1,qmax*nEperp(2)*-1,qmax*nEperp(3)*-1,qmax*nEperp(1)*2,qmax*nEperp(2)*2,qmax*nEperp(3)*2,'color',colors_quiv_Eperp,'linewidth',2)
+    %text(hca,qmax*nEperp(1)*1,qmax*nEperp(2)*1,qmax*nEperp(3)*1,'E_\perp','color',colors_quiv_Eperp,'fontweight',fontweight,'fontsize',fontsize)
+    text(hca,qmax*nEperp(1)*1,qmax*nEperp(2)*1,qmax*nEperp(3)*1,['E_\perp' sprintf('=%.0fmV/m',norm(Eperp))],'color',colors_quiv_Eperp,'fontweight',fontweight,'fontsize',fontsize)
+    
+    quiver3(hca,qmax*nExB(1)*-1,qmax*nExB(2)*-1,qmax*nExB(3)*-1,qmax*nExB(1)*2,qmax*nExB(2)*2,qmax*nExB(3)*2,'color',colors_quiv_ExB,'linewidth',2)    
+    text(hca,qmax*nExB(1)*1,qmax*nExB(2)*1,qmax*nExB(3)*1,'E\times{B}','color',colors_quiv_ExB,'fontweight',fontweight,'fontsize',fontsize)
+    %text(hca,qmax*nExB(1)*1,qmax*nExB(2)*1,qmax*nExB(3)*1,sprintf('E\times{B}=%.0fmV/m',norm(Eperp)),'color',colors_quiv_ExB,'fontweight',fontweight,'fontsize',fontsize)
+
+    hold(hca,'off')
+    %set(hca,'colororder',colors_quiv)
+    %irf_legend(hca,{'B','E','E_\perp','E\times{B}'},[0.98 0.98])
+  end
+  if 0 % plot LMN (obs in GSE!!)
+    %%
+     if 1
+      L_ = L*hs.Rotation';
+      M_ = M*hs.Rotation';
+      N_ = N*hs.Rotation';
+      
+     end
+
+    hold(hca,'on')
+    colors_quiv_LMN = [0.5 0.5 0.5];
+    quiver3(hca,qmax*L_(1)*-1,qmax*L_(2)*-1,qmax*L_(3)*-1,qmax*L_(1)*2,qmax*L_(2)*2,qmax*L_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*L_(1)*1,qmax*L_(2)*1,qmax*L_(3)*1,'L','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*M_(1)*-1,qmax*M_(2)*-1,qmax*M_(3)*-1,qmax*M_(1)*2,qmax*M_(2)*2,qmax*M_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*M_(1)*1,qmax*M_(2)*1,qmax*M_(3)*1,'M','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    quiver3(hca,qmax*N_(1)*-1,qmax*N_(2)*-1,qmax*N_(3)*-1,qmax*N_(1)*2,qmax*N_(2)*2,qmax*N_(3)*2,'color',colors_quiv_LMN,'linewidth',2)
+    text(hca,qmax*N_(1)*1,qmax*N_(2)*1,qmax*N_(3)*1,'N','color',colors_quiv_LMN,'fontweight',fontweight,'fontsize',fontsize)
+
+    hold(hca,'off')
+
+  end
+end
+
+for ip = 1:numel(h)
+  h(ip).XLabel.String = 'v_L (km/s)';
+  h(ip).YLabel.String = 'v_M (km/s)';
+  h(ip).ZLabel.String = 'v_N (km/s)';
+  vlim = [-9 9]*1e3;
+  vlim = 10*[-1 1]*1e3;
   h(ip).XLim = vlim;
   h(ip).YLim = vlim;
   h(ip).ZLim = vlim;
