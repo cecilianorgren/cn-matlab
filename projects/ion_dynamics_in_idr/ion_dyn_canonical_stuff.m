272a4b733d2347a6d94ae70ac325a82e3a86ef1d
Cecilia Norgren <cecilia.norgren@uib.no>
2024-07-08 16:13:56 +0200

    tiny

--- projects/ion_dynamics_in_idr/ion_dyn_canonical_stuff.m
+++ projects/ion_dynamics_in_idr/ion_dyn_canonical_stuff.m
@@ -7,18 +7,31 @@
 
 Lz = 1;
 By0 = 1;
-Ez0 = 1;
+Ez0 = 3;
+q = 1;
+m = 1;
 
-Ax = @(z) -z.^2/2*By0/Lz;
+
+Ax = @(z) z.^2/2*By0/Lz;
 phi = @(z) Ez0*z.^2/2/Lz;
+
+By = @(z) z*By0/Lz; % By = (d/dz)Ax;
 Ez = @(z) -Ez0*z/Lz;
 
-By = @(z) z*By0/Lz; % By = -dAx/dz;
+z0 = 2;
+vx0 = 0;
 
+vx = @(z) vx0 + (By0/Lz)*(z.^2-z0.^2);
 
-zvec = linspace(-3*Lz,3*Lz,100);
+dUpot = @(z) 2*(phi(z)-phi(z0));
 
-nrows = 3;
+vz2 = @(z) -dUpot(z) - vx(z).^2;
+
+vz2(0)
+
+zvec = 2*linspace(-Lz,Lz,100);
+
+nrows = 4;
 ncols = 1;
 h = setup_subplots(nrows,ncols);
 isub = 1;
@@ -35,3 +48,12 @@ hca.XLabel.String = 'z (...)';
 irf_legend(hca,{'\phi','E_z'},[0.02 0.98])
 
 
+hca = h(isub); isub = isub + 1;
+plot(hca,zvec,vx(zvec))
+hca.XLabel.String = 'z (...)';
+irf_legend(hca,{'vx'},[0.02 0.98])
+
+hca = h(isub); isub = isub + 1;
+plot(hca,zvec,vx(zvec).^2,zvec,vz2(zvec),zvec,dUpot(zvec))
+hca.XLabel.String = 'z (...)';
+irf_legend(hca,{'vx^2','vz^2','dU_{pot}'},[0.02 0.98])
