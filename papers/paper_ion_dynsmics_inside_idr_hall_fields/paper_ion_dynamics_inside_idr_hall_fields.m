e42c2d13f7b9b6861f0bada65a8221d7431871f1
Cecilia <cecilia.ae.norgren@gmail.com>
2025-03-19 08:48:21 +0100

    conflict

--- papers/paper_ion_dynsmics_inside_idr_hall_fields/paper_ion_dynamics_inside_idr_hall_fields.m
+++ papers/paper_ion_dynsmics_inside_idr_hall_fields/paper_ion_dynamics_inside_idr_hall_fields.m
@@ -1,7 +1,7 @@
 % Load data
 %mms.db_init('local_file_db','/Users/cecilia/Data/MMS');
-mms.db_init('local_file_db','/Users/cno062/Data/MMS');
-%mms.db_init('local_file_db','/Volumes/mms');
+%mms.db_init('local_file_db','/Users/cno062/Data/MMS');
+mms.db_init('local_file_db','/Volumes/mms');
 %db_info = datastore('mms_db');
 
 units = irf_units;
