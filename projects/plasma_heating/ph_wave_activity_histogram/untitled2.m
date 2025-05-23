b245af9fdb250c16a801882fdab6a58f1ec82538
Cecilia <cecilia.ae.norgren@gmail.com>
2025-05-23 17:36:19 +0200

    mini

---
+++ projects/plasma_heating/ph_wave_activity_histogram/untitled2.m
@@ -0,0 +1,4 @@
+%% Power spectrum as a function some variable
+tint = irf.tint('2017-11-14T19:56:00.00Z/2017-11-14T19:58:00.00Z'); % Yuri shock
+tint_zoom = irf.tint('2017-11-14T19:56:55.00Z/2017-11-14T19:57:25.00Z'); % Yuri shock
+  
